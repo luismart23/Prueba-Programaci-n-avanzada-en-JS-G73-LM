f8024dfdadd3bfbe3700abeb6a9bbb6fc2793021
luismart <luismart@live.cl>
2024-03-27 09:28:58 -0400

    imagenes sonidos modal ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,10 +25,11 @@
 
 // Clase Animal y subclases
 class Animal {
-    constructor(nombre, edad, img, sonido) {
+    constructor(nombre, edad, img, imgSrc, sonido) {
         this.nombre = nombre;
         this.edad = edad;
         this.img = img;
+        this.imgSrc = imgSrc;
         this.sonido = sonido;
     }
 
@@ -44,6 +45,10 @@ class Animal {
         return this.img;
     }
 
+    getImgSrc() {
+        return this.imgSrc;
+    }
+
     getSonido() {
         return this.sonido;
     }
@@ -54,8 +59,8 @@ class Animal {
 }
 
 class Leon extends Animal {
-    constructor(nombre, edad, img, sonido) {
-        super(nombre, edad, img, sonido);
+    constructor(nombre, edad, img, imgSrc, sonido) {
+        super(nombre, edad, img, imgSrc, sonido);
     }
 
     reproducirSonido() {
@@ -65,8 +70,8 @@ class Leon extends Animal {
 }
 
 class Lobo extends Animal {
-    constructor(nombre, edad, img, sonido) {
-        super(nombre, edad, img, sonido);
+    constructor(nombre, edad, img, imgSrc, sonido) {
+        super(nombre, edad, img, imgSrc, sonido);
     }
 
     reproducirSonido() {
@@ -76,8 +81,8 @@ class Lobo extends Animal {
 }
 
 class Oso extends Animal {
-    constructor(nombre, edad, img, sonido) {
-        super(nombre, edad, img, sonido);
+    constructor(nombre, edad, img, imgSrc, sonido) {
+        super(nombre, edad, img, imgSrc, sonido);
     }
 
     reproducirSonido() {
@@ -87,8 +92,8 @@ class Oso extends Animal {
 }
 
 class Serpiente extends Animal {
-    constructor(nombre, edad, img, sonido) {
-        super(nombre, edad, img, sonido);
+    constructor(nombre, edad, img, imgSrc, sonido) {
+        super(nombre, edad, img, imgSrc, sonido);
     }
 
     reproducirSonido() {
@@ -98,8 +103,8 @@ class Serpiente extends Animal {
 }
 
 class Aguila extends Animal {
-    constructor(nombre, edad, img, sonido) {
-        super(nombre, edad, img, sonido);
+    constructor(nombre, edad, img, imgSrc, sonido) {
+        super(nombre, edad, img, imgSrc, sonido);
     }
 
     reproducirSonido() {
@@ -125,26 +130,17 @@ class Aguila extends Animal {
         $nombreAnimalModal.text(nombre);
         $edadAnimalModal.text(edad);
         if (imgSrc) {
-            $imagenAnimalModal.data("src", imgSrc); // Almacenar la ruta de la imagen en los datos del elemento
-            $imagenAnimalModal.attr("src", imgSrc); // Establecer la ruta de la imagen directamente en el atributo src
+            $imagenAnimalModal.attr("src", `./assets/img/${imgSrc}`);
         } else {
-            $imagenAnimalModal.attr("src", ""); // Asigna una cadena vacía si imgSrc no está definido
+            $imagenAnimalModal.attr("src", "");
         }
-        $imagenAnimalModal.data("sonido", sonido); // Establecer el sonido en los datos del elemento de imagen
+        $imagenAnimalModal.data("sonido", sonido);
         $modal.modal("show");
     }
 
-
-    // Función para actualizar la vista previa de la imagen según la selección del usuario
-    function actualizarVistaPreviaImagen(animal) {
-        const imgSrc = animal.img ? `./assets/img/${animal.img}` : ""; // Construir la ruta completa a la imagen
-        $preview.html(`<div style="display: flex; justify-content: center; align-items: center; height: 100%;"><img src="${imgSrc}" alt="${animal.nombre}" style="max-width: 100%; max-height: 250px;"></div>`);
-    }
-
-
     // Función para manejar el evento de clic en el botón de audio dentro del modal
     $modal.on("click", ".btn-audio", function () {
-        const sonido = $(this).closest(".modal-content").find("img").data("sonido"); // Obtener el sonido de los datos del elemento de imagen
+        const sonido = $(this).closest(".modal-content").find("img").data("sonido");
         reproducirSonido(sonido);
     });
 
@@ -152,29 +148,89 @@ class Aguila extends Animal {
     function crearElementoAnimal(animal) {
         const animalDiv = document.createElement("div");
         animalDiv.classList.add("col", "col-md-3", "mb-3");
-        const imgSrc = animal.img ? `./assets/img/${animal.img}` : ""; // Verificar si animal.img está definido
+        const imgSrc = animal.img ? animal.imgSrc : "";
         const html = `
-    <div class="card h-100" data-id="preview" data-sonido="${animal.sonido}">
-        <img src="${imgSrc}" class="card-img-top" alt="${animal.nombre}">
-        <div class="card-body">
-            <h5 class="card-title">${animal.nombre}</h5>
-            <p class="card-text">Edad: ${animal.edad}</p>
-            <button class="btn btn-primary ver-detalle">Ver Detalle</button>
-        </div>
-    </div>
-`;
+            <div class="card h-100" data-id="preview" data-sonido="${animal.sonido}">
+                <img src="./assets/img/${imgSrc}" class="card-img-top" alt="${animal.nombre}">
+                <div class="card-body">
+                    <h5 class="card-title">${animal.nombre}</h5>
+                    <p class="card-text">Edad: ${animal.edad}</p>
+                    <button class="btn btn-primary ver-detalle">Ver Detalle</button>
+                </div>
+            </div>
+        `;
         animalDiv.innerHTML = html;
         return animalDiv;
     }
+
+    // Función para manejar el evento de clic en el botón de ver detalle
+    $(document).on("click", ".ver-detalle", function () {
+        const $animalDiv = $(this).closest(".card");
+        const nombre = $animalDiv.find(".card-title").text();
+        const edad = $animalDiv.find(".card-text").text().split(": ")[1];
+        const imgSrc = $animalDiv.find(".card-img-top").attr("src").split("/").pop();
+        const sonido = $animalDiv.data("sonido");
+        mostrarDetallesAnimal(nombre, edad, imgSrc, sonido);
+    });
+
+    // Función para obtener datos del animal de forma asíncrona
+    async function obtenerDatosAnimal(nombre) {
+        try {
+            const response = await fetch("animales1.json");
+            if (!response.ok) {
+                throw new Error("Error al obtener los datos de los animales.");
+            }
+            const data = await response.json();
+            if (!data || !data.animales || !Array.isArray(data.animales)) {
+                throw new Error("El archivo animales.json no tiene el formato esperado.");
+            }
+            const animalData = data.animales.find(animal => animal.nombre && animal.nombre.toLowerCase() === nombre.toLowerCase());
+            if (!animalData) {
+                throw new Error(`No se encontró el animal "${nombre}".`);
+            }
+
+            if (!animalData.imgSrc) {
+                throw new Error(`La propiedad 'imgSrc' no está definida para el animal "${nombre}".`);
+            }
+
+            return animalData;
+        } catch (error) {
+            console.error("Error en obtenerDatosAnimal:", error);
+            mostrarError("Error al obtener los datos del animal.");
+            return null;
+        }
+    }
+
+    // Función para manejar el evento de cambio en la selección de animal para actualizar la vista previa de la imagen
+    $animalInput.change(async function () {
+        const nombreAnimal = $(this).val();
+        const animalData = await obtenerDatosAnimal(nombreAnimal);
+        if (animalData) {
+            actualizarVistaPreviaImagen(animalData);
+        }
+    });
 
     // Función para mostrar mensajes de error al usuario
     function mostrarError(mensaje) {
         alert(mensaje);
     }
 
+    // Función para actualizar la vista previa de la imagen según la selección del usuario
+    function actualizarVistaPreviaImagen(animal) {
+        const imgSrc = animal.imgSrc ? animal.imgSrc : "";
+        $preview.html(`<div style="display: flex; justify-content: center; align-items: center; height: 100%;"><img src="./assets/img/${imgSrc}" alt="${animal.nombre}"
+        style="max-width: 100%; max-height: 250px;"></div>`);
+    }
+
+    // Función para manejar el evento de clic en el botón de reproducción de sonido
+    $modal.on("click", ".btn-audio", function () {
+        const sonido = $(this).closest(".modal-content").find("img").data("sonido");
+        reproducirSonido(sonido);
+    });
+
     // Función para reproducir el sonido
     function reproducirSonido(sonido) {
-        const audioElement = new Audio(sonido);
+        const audioElement = new Audio(`./assets/sonido/${sonido}`);
         audioElement.play();
     }
 
@@ -197,28 +253,25 @@ class Aguila extends Animal {
             return;
         }
 
-        const imagen = animalData.imagen;
-        const sonido = animalData.sonido;
-
         let animal;
         switch (nombreAnimal) {
             case "León":
-                animal = new Leon(nombreAnimal, edadAnimal, imagen, sonido);
+                animal = new Leon(nombreAnimal, edadAnimal, animalData.img, animalData.imgSrc, animalData.sonido);
                 break;
             case "Lobo":
-                animal = new Lobo(nombreAnimal, edadAnimal, imagen, sonido);
+                animal = new Lobo(nombreAnimal, edadAnimal, animalData.img, animalData.imgSrc, animalData.sonido);
                 break;
             case "Oso":
-                animal = new Oso(nombreAnimal, edadAnimal, imagen, sonido);
+                animal = new Oso(nombreAnimal, edadAnimal, animalData.img, animalData.imgSrc, animalData.sonido);
                 break;
             case "Serpiente":
-                animal = new Serpiente(nombreAnimal, edadAnimal, imagen, sonido);
+                animal = new Serpiente(nombreAnimal, edadAnimal, animalData.img, animalData.imgSrc, animalData.sonido);
                 break;
             case "Águila":
-                animal = new Aguila(nombreAnimal, edadAnimal, imagen, sonido);
+                animal = new Aguila(nombreAnimal, edadAnimal, animalData.img, animalData.imgSrc, animalData.sonido);
                 break;
             default:
-                animal = new Animal(nombreAnimal, edadAnimal, imagen, sonido);
+                animal = new Animal(nombreAnimal, edadAnimal, animalData.img, animalData.imgSrc, animalData.sonido);
         }
 
         $animalesContainer.append(crearElementoAnimal(animal));
@@ -227,47 +280,7 @@ class Aguila extends Animal {
         $comentariosInput.val("");
     });
 
-    // Función para manejar el evento de clic en el botón de ver detalle
-    $(document).on("click", ".ver-detalle", function () {
-        const $animalDiv = $(this).closest(".card");
-        const nombre = $animalDiv.find(".card-title").text();
-        const edad = $animalDiv.find(".card-text").text().split(": ")[1];
-        const imgSrc = $animalDiv.find(".card-img-top").attr("src"); // Obtener la ruta de la imagen
-        const sonido = $animalDiv.data("sonido");
-        mostrarDetallesAnimal(nombre, edad, imgSrc, sonido);
-    });
-
-    // Función para obtener datos del animal de forma asíncrona
-    async function obtenerDatosAnimal(nombre) {
-        try {
-            const response = await fetch("animales1.json");
-            if (!response.ok) {
-                throw new Error("Error al obtener los datos de los animales.");
-            }
-            const data = await response.json();
-            if (!data || !data.animales || !Array.isArray(data.animales)) {
-                throw new Error("El archivo animales.json no tiene el formato esperado.");
-            }
-            const animalData = data.animales.find(animal => animal.nombre && animal.nombre.toLowerCase() === nombre.toLowerCase());
-            if (!animalData) {
-                throw new Error(`No se encontró el animal "${nombre}".`);
-            }
-
-            // Asegurarse de que animalData contenga la propiedad 'img'
-            if (!animalData.img) {
-                throw new Error(`La propiedad 'img' no está definida para el animal "${nombre}".`);
-            }
-
-            // Devolver el objeto animal encontrado
-            return animalData;
-        } catch (error) {
-            console.error("Error en obtenerDatosAnimal:", error);
-            mostrarError("Error al obtener los datos del animal.");
-            return null;
-        }
-    }
-
-    // Manejar el evento de cambio en la selección de animal para actualizar la vista previa de la imagen
+    // Función para manejar el evento de cambio en la selección de animal para actualizar la vista previa de la imagen
     $animalInput.change(async function () {
         const nombreAnimal = $(this).val();
         const animalData = await obtenerDatosAnimal(nombreAnimal);
